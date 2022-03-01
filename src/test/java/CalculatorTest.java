ab78409f10c996af1f1c206180f6de8053aac3a5
Rollins, Edward <errollins.jr@gmail.com>
2022-03-01 07:32:09 -0500

    updated test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,4 @@
+import calculator.Calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,6 +23,41 @@ class CalculatorTest {
     @Test
     public void shouldReturnSumOnMultipleNumbers() {
         assertEquals(6, Calculator.add("1,2,3"));
+    }
+
+    @Test
+    public void shouldAcceptNewLineAsValidDelimiter() {
+        assertEquals(6, Calculator.add("1,2\n3"));
+    }
+
+    @Test
+    public void shouldAcceptCustomDelimiterSyntax() {
+        assertEquals(3, Calculator.add("//;\n1;2"));
+    }
+
+    @Test
+    public void customDelimiterCouldBeAlsoAReqExpSpecialChar() {
+        assertEquals(3, Calculator.add("//.\n1.2"));
+    }
+
+    @Test
+    public void shouldRaiseExceptionOnNegatives() {
+        try {
+            Calculator.add("-1,2,3");
+            fail("Exception expected");
+        } catch (RuntimeException ex) {
+            //ok
+        }
+    }
+
+    @Test
+    public void exceptionMessageShouldContainTheNegativeNumber() {
+        try {
+            Calculator.add("-1,2,3");
+            fail("Exception expected");
+        } catch (RuntimeException ex) {
+            assertEquals("Negatives not allowed: -1", ex.getMessage());
+        }
     }
 
 }
